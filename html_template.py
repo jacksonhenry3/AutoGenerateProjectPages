54ac6394dea6ad85d274fe076db8d78181927b09
Jackson Henry <jackson.henry3@gmail.com>
2014-06-10 23:51:46 -0400

    first working prototype and style

---
+++ html_template.py
@@ -0,0 +1,68 @@
+from pyhtml import *
+def generate_page(head,h,main,projects_sidebar):
+	page = html(
+			head,
+			body(
+				h,
+				div(id = 'all')(
+					main,
+					projects_sidebar
+				)
+			)
+	)
+
+	return(page)
+
+
+def make_head(pg_title,favico_url = '',stylesheet = 'main.css'):
+	h = head(
+		title(pg_title),
+		meta(charset='UTF-8'),
+		meta(name="viewport", content="width=device-width, height=device-height"),
+		meta(content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0', name='viewport'),
+		link(rel="shortcut icon", href=favico_url),
+		link(rel='stylesheet', href=stylesheet),
+	)
+	return(h)
+
+def make_projects_side_bar(project_list):
+	projects = []
+	for project in project_list:
+		projects.append(li(a(project)))
+
+	project_ul = ul(
+		projects
+	)
+	sidebar = div(id = 'sidebar')(
+		project_ul
+		)
+	return(sidebar)
+
+def make_main(title,subtitle,content):
+	d = div(id = 'main_container')(div(id = 'main')(
+		img(src = 'C:\Users\Jackson\Documents\GitHub\Abstract_Art_Project\main_img.png'),
+			h1(a(title)),
+			h3(subtitle),
+			p(content)
+		))
+	return(d)
+
+def make_header(name):
+	h = header(id = 'header')(
+
+		ul(
+			li(name+" on github")
+			# li(),
+			# li()
+		)
+		)
+	return(h)
+# project_list = [
+# "Abstract Art Project","AutoGenerateProjectPages","cipherTyper","cloudSimulator","github","graphingCalculator","jacksonhenry3.github.io","kinematics_Solver","newMediaPlayground","PythonRSAtesting","quantumTicTacToe","Recipe_book","sierpinskiTriangle","to_print","ulamSpiral","UtTotum","vector_field","TEST","UtTotemCS"]
+# projects_sidebar = make_projects_side_bar(project_list)
+
+# head = make_head('Test Page')
+
+# main = make_main()
+# h    = make_header('Jackson Henry')
+# generate_page()
--- pyGithubtest.py
+++ pyGithubtest.py
@@ -1,10 +1,15 @@
 from github import Github
+from html_template import *
 from base64 import b64decode
 g = Github("jacksonhenry3", "7ffa7ace35")
 
 repos = g.get_user().get_repos()
+repoList = []
+repo = repos[0]
+for r in repos:
+	repoList.append(r.name.replace('_',' '))
 
-repo = repos[14]
+
 
 def get_file_contents(repo,path):
 	github_file       = repo.get_file_contents(path)
@@ -13,16 +18,25 @@ def get_file_contents(repo,path):
 	return(contents)
 
 
-def make_projectPage(repo):
+def make_project_page(repo):
 	name = repo.name
 	title = name.replace('_',' ')
 	owner = repo.owner
 	desc  = repo.description
 
 	readme = get_file_contents(repo,'README.md')
-	f = open(name+'.html','w')
-	f.write(readme) # python will convert \n to os.linesep
-	f.close() 
-	# print(readme)
+	# generate_page(name,title,title,desc,readme)
 
-make_projectPage(repo)
+	projects_sidebar = make_projects_side_bar(repoList)
+
+	head = make_head('Page')
+
+	main = make_main(title,desc,readme)
+	h    = make_header(owner.name)
+	pg = generate_page(head,h,main,projects_sidebar)
+	pg = str(pg).replace('&lt;',"<").replace('&gt;','>')
+	f = open('generated_pages/test.html','w')
+	f.write(str(pg)) # python will convert \n to os.linesep
+	f.close() 
+
+make_project_page(repo)
